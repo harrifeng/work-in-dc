f458c3d2be415e3cb99da4d94da879538904f352
Harri Feng <harrifeng@gmail.com>
2016-06-30 13:40:19 +0800

    works now

--- frappe_test/custom/prepare.sh
+++ frappe_test/custom/prepare.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-bench init frappe-bench-test --frappe-path /home/travis/frappe_sj
-cp -r /home/travis/frappe_sj/test_sites/test_site ~/frappe-bench-test/sites/
-cd /home/travis/frappe-bench-test
+bench init frappe-bench-jenkins --frappe-path /home/travis/frappe_sj
+cp -r /home/travis/frappe_sj/test_sites/test_site ~/frappe-bench-jenkins/sites/
+cd /home/travis/frappe-bench-jenkins
 bench use test_site
 bench build
 bench set-mariadb-host db
