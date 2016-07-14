9c8b5cea85166fa9a5e7088471783519c28050d3
Harri Feng <harrifeng@gmail.com>
2016-07-14 15:41:03 +0800

    new changes for erpnext

---
+++ sjerpnext/prepare.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+bench init frappe-bench-jenkins --frappe-path /home/travis/frappe_sj
+cp -r /home/travis/frappe_sj/test_sites/test_site ~/frappe-bench-jenkins/sites/
+cd /home/travis/frappe-bench-jenkins
+bench use test_site
+bench build
+bench set-mariadb-host db
+bench reinstall
+bench start&
+sleep 10
+bench --verbose run-tests --driver Firefox
